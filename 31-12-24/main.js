5b811860f4b4b01accbe8ea57983bc1048f78e11
Mustaqeem-tech <mustaqeemsani5@gmail.com>
2024-12-31 12:39:16 +0500

    Add files via upload

---
+++ 31-12-24/main.js
@@ -0,0 +1,11 @@
+let todos = []; 
+let a =prompt("What do you wanna do today") 
+todos.push(a) 
+let b =prompt("What do you wanna Eat today") 
+todos.push(b) 
+let c =prompt("What do you wanna Play today") 
+todos.push(c) 
+let d =prompt("What subject you study today") 
+todos.push(d) 
+console.log("Your-todo-list"); 
+console.log(todos);
